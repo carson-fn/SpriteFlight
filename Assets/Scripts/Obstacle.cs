e4b36c8ff80890c6e907e1d4e07cc7734f1f390b
carson-fn <180336862+carson-fn@users.noreply.github.com>
2026-02-02 23:41:03 -0700

    Upload Project - simple game scene - floating obstacles with random size and velocity

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    public float minSize = 0.5f;
+    public float maxSize = 2.5f;
+
+    public float minSpeed = 50f;
+    public float maxSpeed = 200f;
+
+    public float maxSpinSpeed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        rb = GetComponent<Rigidbody2D>();
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+        Vector2 randomDirection = Random.insideUnitCircle;
+        rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
